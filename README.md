d849f9fc61eb710f3719ef4bb43ac7fd66491d34
Jeffrey Fisher <jegfish@fastmail.com>
2021-08-02 13:24:09 -0400

    Checkpoint music

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Music Jam Project
 
+## Questions for mentor
+- UI design?
+
 ## Don't lose
 - https://www.clairecodes.com/blog/2018-10-10-css-polka-dot-background/
 - https://doodad.dev/dither-me-this/
+- https://www.devbridge.com/articles/tonejs-coding-music-production-guide/
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,39 @@
+// import * as Tone from '/node_modules/tone/build/Tone.js';
+// import * as Tone from 'tone';
+
 let googleUser;
 
-window.onload = (event) => {
-    // Use this to retain user state between html pages.
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log('Logged in as: ' + user.displayName);
-        googleUser = user;
-      } else {
-        // If not logged in, navigate back to login page.
-        window.location = 'index.html'; 
-      };
-    });
-  };
+// window.onload = (event) => {
+//   // Use this to retain user state between html pages.
+//   firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//     console.log('Logged in as: ' + user.displayName);
+//     googleUser = user;
+//     } else {
+//     // If not logged in, navigate back to login page.
+//     window.location = 'index.html'; 
+//     };
+//   });
+// };
+
+const btn = document.querySelector("#startaudio");
+btn.addEventListener("click", (e) => {
+  console.log("hello");
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease("C4", "8n");
+});
+
+const synth = new Tone.Synth().toDestination();
+document.addEventListener("keydown", (e) => {
+  console.log("keydown:", e.code);
+  if (e.code === "KeyJ") {
+    console.log("the j key was pressed!");
+    synth.triggerAttackRelease("C4", "8n");
+  } else if (e.code === "KeyK") {
+    synth.triggerAttackRelease("D4", "8n");
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  console.log("keyup:", e.code);
+});
