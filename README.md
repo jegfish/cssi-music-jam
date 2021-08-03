181f100d1abb847f0cebd60bc068040f0b69e189
Jeffrey Fisher <jegfish@fastmail.com>
2021-08-03 12:44:29 -0400

    Account for key repeat

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - Tips on splitting up work
 
 ## 
-- Touch screen support
+- Touch screen supports
 
 ## Maybe
 - CSS Variables
@@ -16,3 +16,4 @@
 - https://www.clairecodes.com/blog/2018-10-10-css-polka-dot-background/
 - https://doodad.dev/dither-me-this/
 - https://www.devbridge.com/articles/tonejs-coding-music-production-guide/
+- https://tonejs.github.io/docs/r13/Sequence 
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,11 +16,54 @@ let googleUser;
 //   });
 // };
 
+// const soundMap = {
+//   "KeyQ": "", "KeyW": "", "KeyE": "", "KeyR": "", "KeyT": "", "KeyY": "", "KeyU": "", "KeyI": "", "KeyO": "", "KeyP": "",
+//   "KeyA": "", "KeyS": "", "KeyD": "", "KeyF": "", "KeyG": "", "KeyH": "", "KeyJ": "", "KeyK": "", "KeyL": "",
+//   "KeyZ": "", "KeyX": "", "KeyC": "", "KeyV": "", "KeyB": "", "KeyN": "", "KeyM": "",
+// };
+
+const soundMap = {
+  "KeyW": {note: "C#4", on: false}, "KeyE": {note: "D#4", on: false}, "KeyT": {note: "E#4", on: false}, "KeyY": {note: "F#4", on: false}, "KeyU": {note: "G#4", on: false}, "KeyO": {note: "D#5", on: false}, "KeyP": {note: "C#5", on: false},
+  "KeyA": {note: "C4", on: false}, "KeyS": {note: "D4", on: false}, "KeyD": {note: "E4", on: false}, "KeyF": {note: "F4", on: false}, "KeyG": {note: "G4", on: false}, "KeyH": {note: "A4", on: false}, "KeyJ": {note: "B4", on: false}, "KeyK": {note: "C5", on: false}, "KeyL": {note: "D4", on: false},
+  "KeyZ": "", "KeyX": "", "KeyC": "", "KeyV": "", "KeyB": "", "KeyN": "", "KeyM": "",
+};
+
+
+// const soundMap = {
+//   "KeyW": "C#4", "KeyE": "D#4", "KeyT": "E#4", "KeyY": "F#4", "KeyU": "G#4", "KeyO": "D#5", "KeyP": "C#5",
+//   "KeyA": "C4", "KeyS": "D4", "KeyD": "E4", "KeyF": "F4", "KeyG": "G4", "KeyH": "A4", "KeyJ": "B4", "KeyK": "C5", "KeyL": "D4",
+//   "KeyZ": "", "KeyX": "", "KeyC": "", "KeyV": "", "KeyB": "", "KeyN": "", "KeyM": "",
+// };
+
+const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+const drum = new Tone.PluckSynth(Tone.Synth).toDestination();
+
+const now = Tone.now();
+
 function playKey(keycode) {
   const virtkey = document.querySelector(`#${keycode}`);
   highlightKey(virtkey);
-  const synth = new Tone.Synth().toDestination();
-  synth.triggerAttackRelease("C4", "8n");
+  // const synth = new Tone.Synth().toDestination();
+  // synth.triggerAttackRelease("C4", "8n");
+  // const now = Tone.now();
+  console.log("activeVoices:", synth.activeVoices);
+  console.log("maxPolyphony:", synth.maxPolyphony);
+  // if (synth.activeVoices >= synth.maxPolyphony) {
+  //   synth.releaseAll(now);
+  // }
+  if (soundMap[keycode].on) {
+    return;
+  }
+  synth.triggerAttack(soundMap[keycode].note);
+  soundMap[keycode].on = true;
+}
+
+function stopKey(keycode) {
+  const virtkey = document.querySelector(`#${keycode}`);
+  // const now = Tone.now();
+  // synth.triggerRelease(soundMap[keycode], "+0");
+  synth.releaseAll("+0");
+  unhighlightKey(virtkey);
 }
 
 function highlightKey(elem) {
@@ -39,20 +82,13 @@ btn.addEventListener("click", (e) => {
   synth.triggerAttackRelease("C4", "8n");
 });
 
-const synth = new Tone.Synth().toDestination();
+// const synth = new Tone.Synth().toDestination();
 document.addEventListener("keydown", (e) => {
-  console.log("keydown:", e.code);
-  if (e.code === "KeyJ") {
-    console.log("the j key was pressed!");
-    synth.triggerAttackRelease("C4", "8n");
-  } else if (e.code === "KeyK") {
-    synth.triggerAttackRelease("D4", "8n");
-  }
+  // console.log("keydown:", e.code);
   playKey(e.code);
 });
 
 document.addEventListener("keyup", (e) => {
-  console.log("keyup:", e.code);
-  const virtkey = document.querySelector(`#${e.code}`);
-  unhighlightKey(virtkey);
+  // console.log("keyup:", e.code);
+  stopKey(e.code);
 });
